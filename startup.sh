513b151aa5288a66147855dd676de92dfc4632d5
Marek Konieczny <marek.konieczny@gmail.com>
2017-01-09 17:13:07 +0100

    uncommenting shortcuts

--- startup.sh
+++ startup.sh
@@ -55,8 +55,8 @@ if [ -d .emacs.d/ ]; then
 fi
 
 git clone https://github.com/marekko/dotfiles.git
-#ln -sb dotfiles/.screenrc .
-#ln -sb dotfiles/.bash_profile .
-#ln -sb dotfiles/.bashrc .
-#ln -sb dotfiles/.bashrc_custom .
-#ln -sf dotfiles/.emacs.d .
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
